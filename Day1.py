31c91c7568e17f6e3fd87faf67a9d18445c4bfec
vismayakk01@gmail.com <vismayakk01@gmail.com>
2023-08-18 10:15:09 +0530

    python

---
+++ Day1.py
@@ -0,0 +1,7 @@
+#syntax : the style in which we write the code
+#commands:unexicuted python codes
+#the unexicuted codes starts with#
+#the place where data is stored temporaray eg :name="futura"and n=5
+#it start with a letter or underscore do not start with a number
+
+
