30672e1223990da452636cbe7c6924685e3274b7
vismayakk01@gmail.com <vismayakk01@gmail.com>
2023-08-21 10:27:38 +0530

    pyhon2

--- 1Day.py
+++ 1Day.py
@@ -1,6 +1,6 @@
 #syntax : the style in which we write the code
-#commands:unexicuted python codes
-#the unexicuted codes starts with#
+#commands:unexecuted python codes
+#the unexecuted codes starts with#
 #the place where data is stored temporaray eg :name="futura"and n=5
 #it start with a letter or underscore do not start with a number
 
---
+++ 4 inputoutput.py
@@ -0,0 +1,14 @@
+print("hello")
+h=input("input a number:")
+print(h)
+
+
+num1=int(input("enter your first number : "))
+num2=int(input("enter your second number : "))
+d=num1+num2
+print(d)
+ 
+num3=int(input("enter your third number : "))
+num4=int(input("enter your fourth number :"))
+g=num3*num4
+print(g)
